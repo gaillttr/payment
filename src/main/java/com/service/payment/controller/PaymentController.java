18547d92bdebf901a482f3931f735ae14e71d85c
Titaree Jumnongnusorn <96689258+gaillttr@users.noreply.github.com>
2023-05-02 15:56:43 +0700

    Update PaymentController.java

--- src/main/java/com/service/payment/controller/PaymentController.java
+++ src/main/java/com/service/payment/controller/PaymentController.java
@@ -2,7 +2,6 @@ package com.service.payment.controller;
 
 import java.util.List;
 import java.util.NoSuchElementException;
-//import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,38 +37,9 @@ public class PaymentController {
         }
     }
 
-    // @GetMapping("/{paymentId}/status")
-    // public ResponseEntity<Payment> getPayment(@PathVariable String orderStatus) {
-    //     try {
-    //         Payment payment = paymentService.getPayment(orderStatus);
-    //         return new ResponseEntity<Payment>(payment, HttpStatus.OK);
-    //     } catch (NoSuchElementException e) {
-    //         return new ResponseEntity<Payment>(HttpStatus.NOT_FOUND);
-    //     }
-    // }
-
-    //  @PostMapping("/")
-    // public void add(@RequestBody Payment payment) {
-    //     paymentService.savePayment(payment);
-    // }
-
     @PostMapping("/status_payment")
 	public ResponseEntity<Payment> create(@RequestBody Payment payment) {
 		return ResponseEntity.ok(paymentService.process(payment));
 	}
-
-
-    // @PutMapping("/update-payment/{id}")
-    // public ResponseEntity<?> update(@RequestBody Payment payment, @PathVariable int id) {
-    //     try {
-    //         Payment existPayment = paymentService.getPayment(id);
-    //         payment.setId(id);
-    //         paymentService.savePayment(payment);
-	// 		System.out.println("Update a Customer Successfully!");
-    //         return new ResponseEntity<>(HttpStatus.OK);
-    //     } catch (NoSuchElementException e) {
-    //         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    //     }
-    // }
 
 }

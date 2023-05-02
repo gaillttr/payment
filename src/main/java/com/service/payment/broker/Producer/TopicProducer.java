fcfcfc8d436c6f62efa01451d92a4af895dd0548
Titaree Jumnongnusorn <96689258+gaillttr@users.noreply.github.com>
2023-05-02 16:06:04 +0700

    Add files via upload

---
+++ src/main/java/com/service/payment/broker/Producer/TopicProducer.java
@@ -0,0 +1,38 @@
+package com.service.payment.broker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Order;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class TopicProducer {
+    @Value("${topic.name.producer}")
+    private String topicName;
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(Payment payment) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String orderJson = null;
+        try {
+            orderJson = objectMapper.writeValueAsString(payment);
+        } catch (JsonProcessingException e) {
+            log.error("Error in TopicProducer.sendMessage()");
+            log.error(e.getMessage());
+            e.printStackTrace();
+        }
+        log.info("Payment Detail {}", orderJson);
+        System.out.println(
+            String.format("##########\nProduced Payment Recieved -> %s\n##########", orderJson));
+        this.kafkaTemplate.send(topicName, "Produced Payment Recieved -> ",orderJson);
+    }
+}
